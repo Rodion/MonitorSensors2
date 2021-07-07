c3b70a7a5499b383b39f421382312e1bfccd3b62
Tatiana-Gourianova <62667085+Tatiana-Gourianova@users.noreply.github.com>
2021-07-07 20:10:00 +0200

    Add files via upload

--- Web/src/main/java/by/gourianova/monitorsensors/action/sensor/FindSensorAction.java
+++ Web/src/main/java/by/gourianova/monitorsensors/action/sensor/FindSensorAction.java
@@ -17,8 +17,7 @@ public class FindSensorAction implements Action {
     private final static String FIND_PARAMS = "findString";
     private final static String MESSAGE = "message";
     private final static String SENSORS_LIST = "sensorsList";
-    private SensorService sensorService=new SensorService();
-
+    private final SensorService sensorService = new SensorService();
 
 
     @Override
@@ -26,34 +25,38 @@ public class FindSensorAction implements Action {
         Router router = new Router();
 
         String params = request.getParameter(FIND_PARAMS);
-        ArrayList<Sensor> foundedSensorsList=new ArrayList<>();
+        ArrayList<Sensor> sensorsList = new ArrayList<>();
+        ArrayList<Sensor> foundedSensorsList = new ArrayList<>();
+
         Sensor sensor = new Sensor();
 //TODO if is an appropriate util?
 
 
+        String[] words = params.split(" ");
+        try {
+            sensorsList = sensorService.findAll();
 
-
-
-
-            String[] words = params.split(" ");
-        System.out.println(words[0]+words[1]);
-            try {
-                    foundedSensorsList = sensorService.findEntityByTitleAndModel(words[0], words[1]);
-System.out.println(foundedSensorsList.size()+foundedSensorsList.get(0).toString());
-                    //TODO: write findSensorByLocation
-                    //else  {sensor = sensorService.findSensorByLocation(word);
+            for (Sensor sesensor1 : sensorsList) {
+                for (String word : words) {
+                    if (sesensor1.getName().contains(word) ||
+                            sesensor1.getModel().contains(word)) {
+                        //TODO: add? type & unit
+                        foundedSensorsList.add(sesensor1);
+                    } else if (sesensor1.getDescription() != null)
+                        if (sesensor1.getDescription().contains(word)) {
+                            foundedSensorsList.add(sesensor1);
+                        }
+                }
                 request.setAttribute(SENSORS_LIST, foundedSensorsList);
                 router.setPagePath(PageConstant.SENSOR_FIND);
-
-            } catch (ServiceException e) {
-                request.getSession().setAttribute(MESSAGE, e.getMessage());
-                router.setPagePath(PageConstant.ERROR_PAGE);
-                router.setRoute(Router.RouteType.REDIRECT);
-
             }
+        } catch (ServiceException e) {
+            request.getSession().setAttribute(MESSAGE, e.getMessage());
+            router.setPagePath(PageConstant.ERROR_PAGE);
+            router.setRoute(Router.RouteType.REDIRECT);
 
+        }
 
-
-
-            return router;
-        }}
+        return router;
+    }
+}
